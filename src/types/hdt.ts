a488e484e06e99d42b495e309920abfdfe963ed6
LEONARDO MICELLI <leonardomicelli@gmail.com>
2025-07-29 17:39:14 +0200

    feat: add property extensions

--- src/types/hdt.ts
+++ src/types/hdt.ts
@@ -1,16 +1,6 @@
+import { Model } from "./property"
+
 export interface HdtCreateResponse {
     id: string
     models: Array<Model>
-}
-
-export interface Model {
-    properties: Array<Property>
-}
-
-export interface Property {
-    name: string
-    internalName: string
-    description: string
-    id: string
-    type: string
 }
---
+++ src/types/property.ts
@@ -0,0 +1,24 @@
+export interface Model {
+    properties: Array<Property>
+}
+
+export interface Property {
+    name: string
+    internalName: string
+    description: string
+    id: string
+    type: string
+}
+
+export interface HeartRate extends Property {
+    type: "HeartRate"
+    bpm: number
+    timestamp: number
+}
+
+export interface BloodPressure extends Property {
+    type: "BloodPressure"
+    systolic: number
+    diastolic: number
+    timestamp: number
+}

74652e72670bf8eb67a55256076e77b96fb5ecc2
LEONARDO MICELLI <leonardomicelli@gmail.com>
2025-08-01 15:40:51 +0200

    refactor: move types to common file

--- src/types/hdt.ts
+++ src/types/hdt.ts
@@ -1,6 +1,37 @@
-import { Model } from "./property"
+import { Model, Property } from "./property"
 
 export interface HdtCreateResponse {
     id: string
     models: Array<Model>
+}
+
+/**
+ * This interface encapsulates a response to a Status request from the WLDT HTTP adapter.
+ */
+export interface HdtStatusResponse {
+  actions: Array<any>
+  events: Array<any>
+  properties: Array<PropertyResponse>
+  relationships: Array<any>
+}
+
+export const emptyStatusResponse: () => HdtStatusResponse = () => {
+  return {
+    actions: [],
+    events: [],
+    properties: [],
+    relationships: [],
+  }
+}
+
+/**
+ * This interface encapsulates a Property inside the response to a Status request from the WLDT HTTP adapter.
+ */
+export interface PropertyResponse {
+  exposed: boolean
+  key: string
+  readable: boolean
+  type: string
+  value: Property
+  writable: boolean
 }
